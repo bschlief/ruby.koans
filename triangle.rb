8160e310d7515bf3638c48a92f0ddfc943ff4eb2
Bryan Schlief <bschlief@gmail.com>
2012-07-31 19:56:45 -0700

    Fix triangle.rb, move triangle methods out of the test.

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,28 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if a <= 0 && b <= 0 && c <= 0
+    raise TriangleError, "Can't have a triangle with 0 or smaller length sides"
+  end
+
+  if a + b <= c
+    raise TriangleError, "C is larger than sides A and B together"
+  elsif a + c <= b
+    raise TriangleError, "B is larger than sides A and C together"
+  elsif b + c <= a
+    raise TriangleError, "A is larger than sides B and C together"
+  end
+
+  if a == b && b == c
+    return :equilateral
+  elsif a == b ||  a == c || b == c
+    return :isosceles
+  elsif (a != b) && (a != c) && (b != c)
+    return :scalene
+  else
+   return :unknown
+  end
+
 end
 
 # Error class used in part 2.  No need to change this code.
